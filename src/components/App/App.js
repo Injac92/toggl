fd0ee114e1fdb674d305e2e69a2c53920f965d8b
bikilone <milosbirovljevic@gmail.com>
2020-01-02 22:00:00 +0100

    Added react router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LandingPage from "../LandingPage/LandingPage";
+import Login from "../Login/Login";
+import Signup from "../Signup/Signup";
+import Dashboard from "../Dashboard/Dashboard";
 
 function App() {
-  return <div className="App">dsaf</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route path="/" component={LandingPage} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
