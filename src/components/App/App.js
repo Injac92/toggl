2154d9096f4c5f4fd5f519ed9061c89da4e2769b
bikilone <milosbirovljevic@gmail.com>
2020-01-02 08:35:36 +0100

    Testing for es lint

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,8 @@
 import React from "react";
-
+const x = 12;
+const obj = { a: "SDFA" };
 function App() {
-  return <div className="App">dsaf</div>;
+  return <div className="App">dsdafa asfasdfa adsfasdf af</div>;
 }
 
 export default App;
