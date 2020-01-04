925bd386317ac3da10aaa38d55049d90abcd0269
Injac92 <injacigor92@gmail.com>
2020-01-04 16:21:31 +0100

    ispravka navigacije

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,20 +5,22 @@ import { Link } from "react-router-dom";
 function Navigation() {
   return (
     <div className="navigation">
-      <div className="navigation--info">
-        <img className="navigation--info--img" src={logo} alt="logo" />
-        <h2 className="navigation--info--h2">toggl clone</h2>
+      <div className="brand">
+        <Link to="#">
+          <img className="brand--img" src={logo} alt="logo" />
+        </Link>
+        <h2 className="brand--h2">toggl clone</h2>
       </div>
-      <div className="navigation--hamburger">
-        <div className="navigation--hamburger--ham1"></div>
-        <div className="navigation--hamburger--ham2"></div>
+      <div className="hamburger">
+        <div className="hamburger--ham1"></div>
+        <div className="hamburger--ham2"></div>
       </div>
-      <div className="navigation--links">
+      <div className="menu">
         <Link to="">Feature</Link>
         <Link to="">Pricing</Link>
         <Link to="">Training</Link>
-        <Link to="">Log in</Link>
-        <Link to="">Sing up</Link>
+        <Link to="/login">Log in</Link>
+        <Link to="/signup">Sign up</Link>
       </div>
     </div>
   );
