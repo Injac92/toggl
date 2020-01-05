ecdb35fb531947b34c64e26f6acf63b7fa7c2dca
bikilone <milosbirovljevic@gmail.com>
2020-01-05 11:57:10 +0100

    Izbrisan komentar

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -2,9 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Menu(props) {
-  //   const { isOpen } = props;
-  //   console.log(isOpen);
-  //   const menuClass = isOpen ? "menu--modal" : "menu";
   return (
     <div className="menu">
       <Link to="">Feature</Link>
